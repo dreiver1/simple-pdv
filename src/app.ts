76fb104e5183e7dd77f9ef6006246d5abb4c3b9b
David <viniciosdavid1@gmail.com>
2024-07-04 10:10:33 -0300

    adicionando upload de imagens

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,21 @@
-const Express = require('express')
+const express = require('express')
 import Category from "./routers/category"
 import Product from "./routers/product"
 import Item from "./routers/item"
 import Order from "./routers/order"
+import File from './routers/file'
 import cors = require('cors')
 const morgan = require('morgan')
 
-const app = Express()
+const app = express()
+app.use('/files', express.static('files'))
 app.use(morgan('dev'))
-app.use(Express.json())
+app.use(express.json())
 app.use(cors())
 app.use('/category', Category)
 app.use('/product', Product)
 app.use('/item', Item)
 app.use('/order', Order)
+app.use('/', File)
 
 export default app
---
+++ src/routers/file.ts
@@ -0,0 +1,28 @@
+import multer from 'multer'
+import path from 'path'
+
+import { Router } from 'express'
+
+const storeage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, path.resolve('files'))
+    },
+    filename: (req, file, callback)=> {
+        const time = new Date().getTime()
+
+        callback(null, `${time}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage: storeage })
+const route = Router()
+
+route.post('/upload', upload.single('file'), (req, res) => {
+    return res.json(req.file?.path)
+})
+
+
+export default route
+
+
+
